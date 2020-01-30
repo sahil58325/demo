e445c0169db118f0cdc3aa4120c154b92969d378
Sahil Gupta <Sahil.Gupta@bd.com>
2020-01-30 15:02:21 +0530

    demo

---
+++ UnitTestProjectdemo12/UnitTestProjectdemo12/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectdemo12
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals(true, true);
+               
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.Equals(true, false);
+
+        }
+    }
+}
